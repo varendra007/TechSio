b8bd53903290e993b5cc15c7a8c0c3d1f4f40f87
Varendra Maurya <varendramaurya@gmail.com>
2021-10-01 13:00:34 +0530

    chat and prfile page complete

---
+++ chat-page/script.js
@@ -0,0 +1,115 @@
+console.log(window.innerHeight);
+console.log('lsdkfj');
+
+const chatScreen = document.querySelector('.chat__root');
+chatScreen.style.height = `${window.innerHeight}px`;
+
+/* 
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chat window</title>
+</head>
+<style>
+    body {
+        margin: 0;
+        overflow: hidden;
+    }
+
+    .chat__root {
+        display: flex;
+        width: 100%;
+    }
+
+    .chat__left-column {
+        background-color: red;
+        flex: 0.6;
+        height: 100%;
+        overflow: hidden;
+
+    }
+
+    .chat__left-column-child {
+        overflow-y: scroll;
+        height: 100%;
+        box-sizing: content-box;
+        width: 100%;
+        padding-right: 17px;
+    }
+
+    .chat__right-column {
+        background-color: blue;
+        flex: 0.7;
+        overflow: hidden;
+        height: 100%;
+
+    }
+
+    .chat__right-column-child {
+        overflow-y: scroll;
+        height: 100%;
+        box-sizing: content-box;
+        width: 100%;
+        padding-right: 17px;
+    }
+</style>
+
+<body>
+    <div class="chat__root">
+
+        <div class="chat__left-column">
+
+            <div class="chat__left-column-child">
+
+                <h1>ihiflakdf</h1>
+                <h1>ihiflakdflakfj</h1>
+                <h1>ihiflakdflakfjlakjdf</h1>
+                <h1>ihi</h1>
+                <h1>ihiflakdf</h1>
+                <h1>ihiflakdflakfj</h1>
+                <h1>ihiflakdflakfjlakjdf</h1>
+                <h1>ihi</h1>
+                <h1>ihiflakdf</h1>
+                <h1>ihiflakdflakfj</h1>
+                <h1>ihiflakdflakfjlakjdf</h1>
+                <h1>ihi</h1>
+                <h1>ihiflakdf</h1>
+                <h1>ihiflakdflakfj</h1>
+                <h1>ihiflakdflakfjlakjdf</h1>
+                <h1>ihi</h1>
+                <h1>ihiflakdf</h1>
+                <h1>ihiflakdflakfj</h1>
+                <h1>ihiflakdflakfjlakjdf</h1>
+                <h1>ihi</h1>
+                <h1>ihiflakdf</h1>
+                <h1>ihiflakdflakfj</h1>
+                <h1>ihiflakdflakfjlakjdf</h1>
+                <h1>ihi</h1>
+                <h1>ihiflakdf</h1>
+                <h1>ihiflakdflakfj</h1>
+                <h1>ihiflakdflakfjlakjdf</h1>
+                <h1>ihi</h1>
+                <h1>ihiflakdf</h1>
+                <h1>ihiflakdflakfj</h1>
+                <h1>ihiflakdflakfjlakjdf</h1>
+
+            </div>
+
+        </div>
+        <div class="chat__right-column">
+            <div class="chat__right-column-child">
+
+            </div>
+        </div>
+    </div>
+    <script src="script.js" async defer></script>
+</body>
+
+</html> -->
+
+*/
