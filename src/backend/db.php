6d9f35f0fc1fe8dbca18af0beb7ae3f081ad5f73
Varendra Maurya <varendramaurya@gmail.com>
2021-10-31 17:30:48 +0530

    created dynamic post_manager table in db.php

--- src/backend/db.php
+++ src/backend/db.php
@@ -18,10 +18,13 @@
 
     $sql2 = mysqli_query($conn,"CREATE TABLE `chat`.`messages` ( `msg_id` INT NOT NULL ,  `incoming_msg_id` INT(255) NOT NULL ,  `outgoing_msg_id` INT(255) NOT NULL ,  `message` VARCHAR(10000) NOT NULL ) ENGINE = InnoDB");
 
-    $sql3 = mysqli_query($conn,"CREATE TABLE `chat`.`posts` ( `post_id` INT NOT NULL ,  `user_id` INT(255) NOT NULL ,  `image` VARCHAR(255) NOT NULL ,  `likes` INT NOT NULL ,  `comment` VARCHAR(200) NOT NULL ,  `date` DATE NOT NULL ) ENGINE = InnoDB");
+    $sql3 = mysqli_query($conn,"CREATE TABLE `chat`.`posts` (`post_id` INT NOT NULL AUTO_INCREMENT, `user_id` INT(255) NOT NULL , `image` VARCHAR(255) NOT NULL , `likes` INT NOT NULL , `comment` VARCHAR(200) NOT NULL , `date` DATE NOT NULL, PRIMARY KEY (`post_id`) ) ENGINE = InnoDB");
 
     $sql4 = mysqli_query($conn,"CREATE TABLE `chat`.`users` ( `id` INT NOT NULL ,  `unique_id` INT(255) NOT NULL ,  `fname` VARCHAR(200) NOT NULL ,  `lname` VARCHAR(100) NOT NULL ,  `email` VARCHAR(200) NOT NULL ,  `password` VARCHAR(200) NOT NULL ,  `image` VARCHAR(400) NOT NULL ,  `status` VARCHAR(100) NOT NULL ) ENGINE = InnoDB");
 
     $sql5 = mysqli_query($conn,"CREATE TABLE `chat`.`user_info` ( `id` INT NOT NULL ,  `user_id` INT(255) NOT NULL ,  `status` VARCHAR(200) NOT NULL ,  `education` VARCHAR(200) NOT NULL ,  `address` VARCHAR(300) NOT NULL ,  `website` VARCHAR(500) NOT NULL ,  `hobbies` VARCHAR(400) NOT NULL, `cover_image` VARCHAR(400) NOT NULL ) ENGINE = InnoDB");
+
+    $sql6 = mysqli_query($conn, "CREATE TABLE `chat`.`post_manager` ( `id` INT NOT NULL , `post_id` INT NOT NULL , `user_id` INT NOT NULL ) ENGINE = InnoDB");
 $conn->close();
+
 ?>
